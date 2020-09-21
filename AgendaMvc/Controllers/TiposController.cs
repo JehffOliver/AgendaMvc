91d218c83ec5e60adcdeda6d827c6587ab701736
Jefferson <jehff.aral@hotmail.com>
2020-09-21 01:36:45 -0300

    Criando o controler e a View

---
+++ AgendaMvc/Controllers/TiposController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using AgendaMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgendaMvc.Controllers
+{
+    public class TiposController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<TipoContato> list = new List<TipoContato>();
+
+            list.Add(new TipoContato {Id = 1, Name = "Amigos" });
+            list.Add(new TipoContato {Id = 2, Name = "Trabalho" });
+            list.Add(new TipoContato {Id = 3, Name = "Casa" });
+            list.Add(new TipoContato {Id = 4, Name = "Outro" });
+
+            return View(list);
+        }
+    }
+}
